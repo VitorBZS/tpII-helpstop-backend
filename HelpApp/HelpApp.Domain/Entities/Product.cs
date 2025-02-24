a12ca9a464083486220974b3e6408f7be821e121
VitorBZS <bonilhazanattasilva@gmail.com>
2025-02-24 12:34:20 -0300

    Criação Básica dos domínios de entidade

---
+++ HelpApp/HelpApp.Domain/Entities/Product.cs
@@ -0,0 +1,28 @@
+﻿namespace HelpApp.Domain.Entities
+{
+    public class Product
+    {
+        #region Atributos
+        public int Id{ get; set; }
+        public string Name{ get; set; }
+        public string Descripcion{ get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public string Image { get; set; }
+        public string CategoryId{ get; set; }
+        #endregion
+
+        #region Construtores
+        public Product(string name, string description, decimal price, int stock, string image)
+        {
+            Name = name;
+            Descripcion = description;
+            Price = price;
+            Stock = stock;
+            Image = image;
+        }
+
+        public Category category { get; set; }
+        #endregion
+    }
+}
